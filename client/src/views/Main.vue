d3b2e9fb70575eeea469a97b3c96b2dcf5713bb3
Šarūnas Lekstutis <sarunas.lekstutis@gmail.com>
2021-11-06 09:31:42 +0200

    small adjustment of main layout

--- client/src/views/Main.vue
+++ client/src/views/Main.vue
@@ -1,12 +1,13 @@
 <template>
-  <div class="homepage-wrapper">
-    <div class="home-content">
+  <div class="homepage">
+    <div class="homepage__content">
       <h1>Basic Users System</h1>
       <h3>Home Page</h3>
       <h5>
         Demo page of signup/signin users page where logged in person can see all
         users registered.
       </h5>
+      <div class="homepage__content__paragraph">
       <p>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate
         numquam cum consequuntur quos exercitationem. Id inventore voluptate
@@ -15,32 +16,31 @@
         accusamus voluptate quo vitae cum optio veritatis quam ea. Laudantium,
         provident. Aliquam, non voluptas?
       </p>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate
         numquam cum consequuntur quos exercitationem. Id inventore voluptate
         blanditiis vero omnis. Doloribus ipsam dolorem quibusdam ad quae
         officia, culpa quia mollitia quasi quis iure adipisci corporis, non
         accusamus voluptate quo vitae cum optio veritatis quam ea. Laudantium,
-        provident. Aliquam, non voluptas?Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate
+        provident. Aliquam, non voluptas?
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate
         numquam cum consequuntur quos exercitationem. Id inventore voluptate
         blanditiis vero omnis. Doloribus ipsam dolorem quibusdam ad quae
         officia, culpa quia mollitia quasi quis iure adipisci corporis, non
         accusamus voluptate quo vitae cum optio veritatis quam ea. Laudantium,
-        provident. Aliquam, non voluptas?Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate
+        provident. Aliquam, non voluptas?
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate
         numquam cum consequuntur quos exercitationem. Id inventore voluptate
         blanditiis vero omnis. Doloribus ipsam dolorem quibusdam ad quae
         officia, culpa quia mollitia quasi quis iure adipisci corporis, non
         accusamus voluptate quo vitae cum optio veritatis quam ea. Laudantium,
-        provident. Aliquam, non voluptas?Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate
-        numquam cum consequuntur quos exercitationem. Id inventore voluptate
-        blanditiis vero omnis. Doloribus ipsam dolorem quibusdam ad quae
-        officia, culpa quia mollitia quasi quis iure adipisci corporis, non
-        accusamus voluptate quo vitae cum optio veritatis quam ea. Laudantium,
-        provident. Aliquam, non voluptas?Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate
-        numquam cum consequuntur quos exercitationem. Id inventore voluptate
-        blanditiis vero omnis. Doloribus ipsam dolorem quibusdam ad quae
-        officia, culpa quia mollitia quasi quis iure adipisci corporis, non
-        accusamus voluptate quo vitae cum optio veritatis quam ea. Laudantium,
-        provident. Aliquam, non voluptas?</p>
+        provident. Aliquam, non voluptas?
+      </p>
+      </div>
     </div>
   </div>
 </template>
